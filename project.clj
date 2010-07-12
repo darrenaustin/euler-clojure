099825bb10edf2c09fd6373a715f68cf1d8b8f56
Darren Austin <darren.austin@gmail.com>
2010-07-11 18:13:05 -0700

    Initial creation of the euler-clojure project.

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject puzzle-agent "1.0.0-SNAPSHOT"
+  :description "Programs to solve Project Euler problems"
+  :dependencies [[org.clojure/clojure "1.1.0"]
+                 [org.clojure/clojure-contrib "1.1.0"]]
+  :dev-dependencies [[swank-clojure "1.2.1"]])
---
+++ test/euler_clojure/core_test.clj
@@ -0,0 +1,6 @@
+(ns euler-clojure.core-test
+  (:use [euler-clojure.core] :reload-all)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false))
