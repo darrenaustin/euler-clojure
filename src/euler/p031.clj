069fedfbcc74f6f2e14c2f4c6e3de8aad7a13eb2
Darren Austin <darren.austin@gmail.com>
2010-07-26 03:11:42 -0700

    Found a cleaner way to solve 31.

--- src/euler/p031.clj
+++ src/euler/p031.clj
@@ -1,14 +1,13 @@
 (ns euler.p031)
 
-(defn ways [n parts]
-  (letfn [(count-ways [n max-part-index]
-                      (let [part (nth parts max-part-index)]
-                        (cond (> part n) 0
-                              (= part n) 1
-                              :else (+ (count-ways (- n part) max-part-index)
-                                       (count-ways n (inc max-part-index))))))]
-    (count-ways n 0)))
+(defn ways [n coins]
+  (if (seq coins)
+    (let [coin (first coins)]
+      (cond (> coin n) 0
+            (= coin n) 1
+            :else (+ (ways (- n coin) coins)
+                     (ways n (rest coins)))))
+    0))
 
 (defn solution []
   (ways 200 [1 2 5 10 20 50 100 200]))
-
