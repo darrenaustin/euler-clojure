4299f7aae09d25b1d3b95a4013fd27d4d9372977
Darren Austin <darren.austin@gmail.com>
2010-08-06 23:26:41 -0700

    Solved 45.

---
+++ src/dma/euler/p045.clj
@@ -0,0 +1,11 @@
+(ns dma.euler.p045
+  (:use dma.euler.numeric))
+
+(defn solution {:answer 1533776805} []
+  (first
+   (for [n (iterate inc 286)
+         :let [t (/ (* n (inc n)) 2)
+               pt (/ (+ 1/2 (Math/sqrt (+ 1/4 (* 6 t)))) 3)
+               ht (/ (+ 1 (Math/sqrt (inc (* 8 t)))) 4)]
+         :when (and (= pt (int pt)) (= ht (int ht)))]
+     t)))
