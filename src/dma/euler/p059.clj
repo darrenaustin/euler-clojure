6e0b950c6c8897ffca297250d8141162af2e9b48
Darren Austin <darren.austin@gmail.com>
2010-08-08 23:00:14 -0700

    Solved 59

---
+++ src/dma/euler/p059.clj
@@ -0,0 +1,12 @@
+(ns dma.euler.p059
+  (:use dma.euler.numeric))
+
+(def lower-case (range (int \a) (inc (int \z))))
+
+(defn solution {:answer 107359} []
+  (let [encrypted (map #(Integer/parseInt %)  (.split (.trim (slurp "data/cipher1.txt")) ","))]
+    (sum (map int
+              (first
+               (filter #(re-find #"\sthe\s.*\sthe\s" %)
+                       (for [c1 lower-case, c2 lower-case, c3 lower-case]
+                         (apply str (map #(char (bit-xor %1 %2)) encrypted (cycle [c1 c2 c3]))))))))))
