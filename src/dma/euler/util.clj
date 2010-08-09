b14bb0213ec6208e85509deda45dcd41bfbde942
Darren Austin <darren.austin@gmail.com>
2010-08-08 17:24:49 -0700

    Solved poker problem 54

--- src/dma/euler/util.clj
+++ src/dma/euler/util.clj
@@ -5,3 +5,16 @@
 
 (defn collect [cols]
   (mapcat identity cols))
+
+(defn indexed-map
+  ([keys] (indexed-map keys 0))
+  ([keys start-value]
+     (apply hash-map (interleave keys (iterate inc start-value)))))
+
+(defn compare-seqs [s1 s2]
+  (if (empty? s1)
+    (compare nil (first s2))
+    (let [comparison (compare (first s1) (first s2))]
+      (if (zero? comparison)
+        (compare-seqs (rest s1) (rest s2))
+        comparison))))
