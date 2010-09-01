63d80233c0b34982a223831ecc6348b5b645c7a5
Darren Austin <darren@darrenaustin.com>
2010-09-01 01:55:39 -0700

    Ported 71.

---
+++ src/dma/euler/p071.clj
@@ -0,0 +1,13 @@
+(ns dma.euler.p071)
+
+(defn solution {:answer 428570} []
+  (numerator
+   (reduce (fn [closest d]
+             (let [n (quot (* d 3) 7)
+                   f (/ n d)]
+               (if (< closest f 3/7)
+                 f
+                 closest)))
+           0 (range 2 1000001))))
+
+
