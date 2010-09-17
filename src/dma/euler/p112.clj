aa701bfdd23aceb17aa1398f4418901e8c4de65f
Darren Austin <darren@darrenaustin.com>
2010-09-17 15:36:44 -0700

    Cleaned up solution for 112.

--- src/dma/euler/p112.clj
+++ src/dma/euler/p112.clj
@@ -1,19 +1,33 @@
 (ns dma.euler.p112
   (:use dma.euler.numeric))
 
+(defn increasing? [digits]
+  (apply <= digits))
+
+(defn decreasing? [digits]
+  (apply >= digits))
+
 (defn bouncy? [n]
-  (let [ds (digits n)
-        increasing? (fn [ds] (apply <= ds))
-        decreasing? (fn [ds] (apply >= ds))]
+  (let [ds (digits n)]
     (and (not (increasing? ds))
          (not (decreasing? ds)))))
 
-(defn least-bouncy-proportion [target]
-  (loop [num-bouncy 0
-         n 100]
-    (if (= (/ num-bouncy (dec n)) target)
-      (dec n)
-      (recur (if (bouncy? n) (inc num-bouncy) num-bouncy) (inc n)))))
+;; More functional, but slower approach.  Would like to see if
+;; there is a more concise way to do this
+;;
+;; (defn least-bouncy-proportion [target]
+;;   (second (first (filter (fn [[num-bouncy n]] (= (/ num-bouncy n) target))
+;;                          (reductions
+;;                           (fn [[num-bouncy n] x]
+;;                             [(if (bouncy? x) (inc num-bouncy) num-bouncy) x])
+;;                           [0 99]
+;;                           (iterate inc 100))))))
 
-(defn solution {:answer nil} []
+(defn least-bouncy-proportion [target]
+  (loop [num-bouncy 0, n 100]
+    (if (= (/ num-bouncy n) target)
+      n
+      (recur (if (bouncy? (inc n)) (inc num-bouncy) num-bouncy) (inc n)))))
+
+(defn solution {:answer 1587000} []
   (least-bouncy-proportion 99/100))
