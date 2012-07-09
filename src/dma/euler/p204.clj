38d84e8ce6629757857612c0d251b5b218df6c11
Darren Austin <darren@darrenaustin.org>
2012-07-08 18:05:49 -0700

    Checked in work in progress for 96, 111, 204.

---
+++ src/dma/euler/p204.clj
@@ -0,0 +1,9 @@
+(ns dma.euler.p204
+  (:use [dma.euler numeric primes]))
+
+(defn hamming? [n max-prime-factor]
+  (every? #(<= % max-prime-factor) (prime-factors n)))
+
+(defn solution {:answer nil} []
+  (count
+   (take-while-< (exp 10 5) (filter #(hamming? % 5) (natural-nums)))))
